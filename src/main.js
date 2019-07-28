8e118abfc55d84cd240172b0a0fcf7cfff5475d5
Knight <177034@qq.com>
2019-07-29 00:17:26 +0800

    rootvalue动态设置完成

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import store from './store'
 // 引入vant -ui
 import Vant from 'vant'
 import 'vant/lib/index.css'
+// 引入amfe-flex-ible设置rem基准值
+import 'amfe-flexible/index.js'
 Vue.use(Vuex)
 Vue.use(Vant)
 Vue.config.productionTip = false
